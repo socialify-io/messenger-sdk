5f348c5de9003b38c0771ede507c5847385886f6
Tomasz F <tomekftomekf@gmail.com>
2021-05-29 22:44:45 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,14 +14,13 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/scinfu/SwiftSoup", from: "2.3.2"),
-        .package(url: "https://github.com/belozierov/SwiftCoroutine", from: "2.1.10")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MessengerSdk",
-            dependencies: ["SwiftSoup", "SwiftCoroutine"]),
+            dependencies: ["SwiftSoup"]),
         .testTarget(
             name: "MessengerSdkTests",
             dependencies: ["MessengerSdk"]),
