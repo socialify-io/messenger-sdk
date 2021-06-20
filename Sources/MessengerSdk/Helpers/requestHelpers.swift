1675cb3553d2aecbbb658f1a16cf6be302df2533
Pengwius <tomekftomekf@gmail.com>
2021-06-20 20:19:17 +0200

    Rename requestHelper to requestHelpers

--- Sources/MessengerSdk/Helpers/requestHelpers.swift
+++ Sources/MessengerSdk/Helpers/requestHelpers.swift
@@ -1,18 +1,16 @@
 //
-//  helper.swift
+//  requestHelpers.swift
 //  
 //
 //  Created by Tomasz on 19/05/2021.
 //
 
 import Foundation
-import Combine
-import SwiftSoup
 
 @available(iOS 13.0, *)
 extension MessengerClient {
     
-    /// MARK: - Helper functions
+    /// MARK: - Request helpers
     func makeRequest(request: URLRequest, completion: @escaping (Result<Data, MessengerError>) -> Void) {
         var request = request
         request.addValue("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36", forHTTPHeaderField: "User-Agent")
